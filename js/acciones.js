cc0a256b86545e2d343b583d113034401e6e29fa
ICEon <matrix_eon@yahoo.com>
2016-08-25 10:00:23 -0500

    tres
    Acciiones Beep

--- js/acciones.js
+++ js/acciones.js
@@ -5,8 +5,8 @@ document.addEventListener("deviceready", onDeviceReady, false);
 
 
  function onDeviceReady() {
-
-  
-
+	 $("#btn-sonido").on("tap",function(){
+		navigator.notification.beep(2); 
+	 });
  } //device ready
 }); //ready
